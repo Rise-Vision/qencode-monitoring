89b58c46d68c66e3aedcbaec0b05065705a7cdd5
Tyler Johnson <mail@tyler-johnson.ca>
2020-05-17 00:29:19 -0400

    Retrieve access token and create task

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
+const {request, instance} = require("gaxios");
+
+const {
+  ENCODING_API_KEY,
+  ENCODING_BUCKET,               // eslint-disable-line no-unused-vars
+  ENCODING_STORAGE_ACCESS_KEY,   // eslint-disable-line no-unused-vars
+  ENCODING_STORAGE_SECRET        // eslint-disable-line no-unused-vars
+} = process.env;
+
+instance.defaults = {
+  baseURL: "https://api.qencode.com/v1",
+  method: "POST",
+  timeout: 5000,
+  headers: {
+    "content-type": "application/x-www-form-urlencoded"
+  }
+}
+
 exports.beginNewEncodingHealthCheck = (req, resp)=>{
   console.log({body: req.body});
-  resp.send(process.title);
+
+  const jobData = {};
+
+  return request({url: "/access_token", data: `api_key=${ENCODING_API_KEY}`})
+  .then(resp=>Object.assign(jobData, resp.data))
+  .then(()=>request({url: "/create_task", data: `token=${jobData.token}`}))
+  .then(()=>{resp.send(process.title);});
 };
 
 exports.checkJobStatus = (req, resp)=>{
   console.log({body: req.body});
-  resp(process.title);
+  resp.send(process.title);
 };
--- test/test.js
+++ test/test.js
@@ -1,7 +1,39 @@
 const {beginNewEncodingHealthCheck} = require("../src/index.js");
+const {instance} = require("gaxios");
 const assert = require("assert");
 
 describe("New Encoding Health Check", ()=>{
+  const OK = {status: 200, data: ""};
+  const TOKEN = {status: 200, data: {token: "abc"}};
+
+  beforeEach(()=>{instance.defaults.adapter = ()=>OK});
+
+  it("requests an access token", ()=>{
+    return new Promise(res=>{
+      instance.defaults.adapter = (conf)=>{
+        if (conf.url.endsWith("access_token")) {res();}
+        return OK;
+      };
+
+      beginNewEncodingHealthCheck({}, {send: (result)=>{}})
+    });
+  });
+
+  it("creates as task (encoding job)", ()=>{
+    return new Promise(res=>{
+      instance.defaults.adapter = (conf)=>{
+        if (conf.url.endsWith("access_token")) {return TOKEN};
+        if (conf.url.endsWith("create_task")) {
+          assert(conf.data === "token=abc");
+          res();
+        }
+        return OK;
+      };
+
+      beginNewEncodingHealthCheck({}, {send: (result)=>{}})
+    });
+  });
+
   it("calls response callback", (done)=>{
     beginNewEncodingHealthCheck({}, {send: (result)=>{
       assert(result);
